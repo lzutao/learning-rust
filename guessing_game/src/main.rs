eccfbcf6a0781995abe613f36f1664ebb9695f07
Lzu Tao <taolzu@gmail.com>
2018-12-21 19:24:14 +0700

    guessing_game: Add explicit type

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,21 +5,23 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess:");
 
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        //let guess: u32 = match guess.trim().parse() {
+        // or
+        let guess = match guess.trim().parse::<u32>() {
             Ok(num) => num,
-            Err(_) => {
-                println!("--> Invalid input!");
-                continue
+            Err(errmsg) => {
+                println!("--> {}", errmsg);
+                continue;
             },
         };
 
