b242d6ab61e6c2b4af1ec91d8bb208355df80f86
Lzu Tao <taolzu@gmail.com>
2018-12-21 19:24:54 +0700

    variables: Add examples for constant and shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,15 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // Constants
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+    // Shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
 }
