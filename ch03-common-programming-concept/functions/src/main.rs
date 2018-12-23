4477469c3bc22b5487f19f586752dfee595bc3a9
Lzu Tao <taolzu@gmail.com>
2018-12-23 15:06:59 +0700

    Add ch03

--- ch03-common-programming-concept/functions/src/main.rs
+++ ch03-common-programming-concept/functions/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    another_function(5, 6);
+    let (x, y) = (5, 7);
+    another_function(x, y);
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
 
 fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
+    println!("another_function: The value of x is: {}", x);
+    println!("another_function: The value of y is: {}", y);
 }
